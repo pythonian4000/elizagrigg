c6573d763a1435e9d4e03a443ae63b13eba9b472
Jack Grigg <me@jackgrigg.com>
2013-10-06 09:37:39 +1300

    Theme information change

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 			<?php do_action( 'elizagrigg_credits' ); ?>
 			<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'elizagrigg' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'elizagrigg' ), 'elizagrigg', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'elizagrigg' ), 'elizagrigg', '<a href="http://jackgrigg.com/" rel="designer">Jack Grigg</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
