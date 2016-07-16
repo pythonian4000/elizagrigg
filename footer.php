47d69fba159656ee6a054c87e24954e3a9bc486e
Jack Grigg <me@jackgrigg.com>
2016-07-17 10:57:46 +1200

    Fix theme name in footer

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'snowglass' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'snowglass' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'snowglass' ), 'snowglass', '<a href="https://jackgrigg.com/" rel="designer">Jack Grigg</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'snowglass' ), 'Snowglass', '<a href="https://jackgrigg.com/" rel="designer">Jack Grigg</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
