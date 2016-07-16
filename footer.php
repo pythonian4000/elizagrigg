53e9802833c4ffae52d03187bf74cef7086319b4
Jack Grigg <me@jackgrigg.com>
2016-07-17 10:31:03 +1200

    Move footer outside content again, and fix shadowing issue

--- footer.php
+++ footer.php
@@ -11,6 +11,8 @@
 
 ?>
 
+	</div><!-- #content -->
+
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'snowglass' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'snowglass' ), 'WordPress' ); ?></a>
@@ -18,8 +20,6 @@
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'snowglass' ), 'snowglass', '<a href="https://jackgrigg.com/" rel="designer">Jack Grigg</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
-
-	</div><!-- #content -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
